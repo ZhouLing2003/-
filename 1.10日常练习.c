002a32a44b7fea436a8582b5932f9d80f553c29a
周灵 <95582840+ZhouLing2003@users.noreply.github.com>
2022-01-11 00:03:57 +0800

    Add files via upload

---
+++ 1.10日常练习.c
@@ -0,0 +1,86 @@
+#include<stdio.h>
+#define Q "His wife is so beautiful"
+/*
+int main()
+{
+	double s=-123.6; 
+	printf("s=% 6.2f",s);
+	return 0;
+}
+int main()
+{
+	float s=123.2;
+	printf("s=%+6.2f",s);
+	return 0;
+}
+int main()
+{
+	int s=120;
+	printf("s=%#o\n",s);//显示了8进制的符号 
+	printf("s=%#x\n",s);//显示了16进制的符号
+	printf("s=%o\n",s);//不显示8进制的符号 
+	printf("s=%x",s);//不显示16进制的符号 
+    return 0;	
+}
+int main()
+{
+	int a=2,b=123;
+	int d=6;
+	printf("%010d\n",a);
+	printf("%010d\n",b);
+	printf("%5.3d\n",d);
+	return 0;
+}
+int main()
+{
+	printf("\"%s\"",Q);
+	return 0;
+}
+*/
+#define BOOK "War and Peace"
+int main(void)
+{
+	float cost=12.99;
+	float percent=80.0;
+	printf("This copy of \"%s\" sells for $%.2f.\n",BOOK,cost);
+	printf("This is %.0f%% of list.",percent);
+	return 0;
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 1.9日常练习.c
@@ -0,0 +1,148 @@
+#include<stdio.h>
+/*
+int main()
+{
+	int sum=4;
+	printf("%d+%d=%d",sum,sum,sum+sum);
+	return 0;
+}
+int main()
+{
+	printf("zhou ling\n");
+	printf("zhou\nling\n");
+	printf("zhou");
+	printf(" ling");
+	return 0;
+} 
+int main()
+{
+	int days,age;
+	age=19;
+	days=365*age;
+	printf("%d %d",age,days);
+	return 0;
+}
+//函数的声明
+void jolly();
+void deny();
+int main()
+{
+    jolly();
+    jolly();
+    jolly();
+    deny();
+   return 0;	
+}
+void jolly()
+{
+	printf("For he's a jolly good fellow!\n");
+}
+void deny()
+{
+	printf("Which noboody can deny!");	
+}
+//函数的声明 
+void br();
+void ic();
+int main()
+{
+	//函数调用 
+	br();
+	printf(", ");
+	ic();
+	printf("\n");
+	br();
+	printf("\n");
+	ic();	
+	return 0; 
+}
+//函数的定义 
+void br()
+{
+	printf("Brazil, Russia"); 
+}
+void ic()
+{
+	printf("India, China");
+}
+int main()
+{
+	int toes=10,toes_2,squared;
+	toes_2=toes*2;
+	squared=toes*toes;
+	printf("%d的两倍是%d，%d的平方是%d\n",toes,toes_2,toes,squared);
+	return 0;
+}
+//函数声明 
+void smile();
+int main()
+{
+	smile();smile();smile();
+	printf("\n");
+	smile();smile();
+	printf("\n");
+	smile();
+	return 0;
+}
+void smile()
+{
+	printf("Smile！"); 
+}
+*/
+int main()
+{
+	printf("Starting now:\n");
+	one_three();
+	two();
+	printf("three\n");
+	printf("done!");
+	return 0;
+}
+void one_three()
+{
+	printf("one\n");
+}
+void two()
+{
+	printf("two\n");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
